4f0a3597baf2afb6637ddfa6a85f79f4a802e247
vadim <ya.vadim908@yandex.ru>
2020-11-23 20:11:12 +0300

    Объединить ветки

---
+++ javascript/js.js
@@ -0,0 +1,19 @@
+let button = document.querySelector('.profile__button');
+let popup = document.querySelector('.popup');
+let popupButton = document.querySelector('.popup__button');
+let profileAvatar = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle')
+let popupAuthor = document.querySelector('.popup__author');
+let popupCharacteristic = document.querySelector('.popup__characteristic');
+
+
+
+button.addEventListener('click', function(){
+    popup.classList.add('popup__opened');
+})
+
+popupButton.addEventListener('click', function(){ 
+    profileAvatar.value = popupAuthor.value;
+    profileSubtitle.value = popupCharacteristic.value;
+    popup.classList.remove('popup__opened');
+})
