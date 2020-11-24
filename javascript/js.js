d6d54f101d446708527cf94bcc1987eb99157e7f
vadim <ya.vadim908@yandex.ru>
2020-11-24 16:59:39 +0300

    Финальная версия

--- javascript/js.js
+++ javascript/js.js
@@ -5,14 +5,18 @@ let profileAvatar = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle')
 let popupAuthor = document.querySelector('.popup__author');
 let popupCharacteristic = document.querySelector('.popup__characteristic');
-
-
+let popupButtonClose = document.querySelector('.popup__button-close');
 
 button.addEventListener('click', function(){
     popup.classList.add('popup__opened');
 })
 
+popupButtonClose.addEventListener('click', function(){
+    popup.classList.remove('popup__opened');
+})
+
 popupButton.addEventListener('click', function(){ 
+    
     profileAvatar.value = popupAuthor.value;
     profileSubtitle.value = popupCharacteristic.value;
     popup.classList.remove('popup__opened');
