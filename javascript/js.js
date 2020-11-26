63fe977c86ff46d5a3c1f5b70dcb956b62e10c21
vadim <ya.vadim908@yandex.ru>
2020-11-26 13:29:32 +0300

    Исправленная версия

--- javascript/js.js
+++ javascript/js.js
@@ -1,33 +1,30 @@
 let button = document.querySelector('.profile__button');
 let popup = document.querySelector('.popup');
-let popupButton = document.querySelector('.popup__button');
 let profileAvatar = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle')
 let popupAuthor = document.querySelector('.popup__text_author');
 let popupCharacteristic = document.querySelector('.popup__text_characteristic');
 let popupButtonClose = document.querySelector('.popup__button-close');
+let popupForm = document.querySelector('.popup__container');
 
 function openPopup(event){
     event.preventDefault();
     popup.classList.add('popup_opened');
-    popupAuthor.value = profileAvatar.value;
-    popupCharacteristic.value = profileSubtitle.value;
+    popupAuthor.value = profileAvatar.textContent;
+    popupCharacteristic.value = profileSubtitle.textContent;
+}
+
+function buttonClose(){
+    popup.classList.remove('popup_opened');
+}
+
+function popupFormSave(evt){ 
+    evt.preventDefault();
+    profileAvatar.textContent = popupAuthor.value;
+    profileSubtitle.textContent = popupCharacteristic.value;
+    popup.classList.remove('popup_opened');
 }
 
 button.addEventListener('click', openPopup);
-
-function ButtonClose(){
-    
-    popup.classList.remove('popup_opened');
-}
-
-popupButtonClose.addEventListener('click', ButtonClose);
-
-function popupButtonSave(event){ 
-    event.preventDefault()
-    profileAvatar.value = popupAuthor.value;
-    profileSubtitle.value = popupCharacteristic.value;
-    popup.classList.remove('popup_opened');
-}
-
-popupButton.addEventListener('click', popupButtonSave);
+popupButtonClose.addEventListener('click', buttonClose);
+popupForm.addEventListener('submit', popupFormSave);
